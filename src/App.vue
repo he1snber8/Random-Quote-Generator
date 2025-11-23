709da0e15a8ec626969985d363449f98a7a9b28d
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-23 13:59:18 +0400

    Add App.vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="app">
+    <h1>Random Quote Generator</h1>
+    <QuoteView />
+  </main>
+</template>
+
+<script setup>
+import QuoteView from './components/QuoteView.vue';
+</script>
+
+<style>
+.app{min-height:100vh;display:flex;flex-direction:column;align-items:center;justify-content:center;padding:1rem;}
+</style>
