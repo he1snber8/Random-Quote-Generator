75252a4764b28637801fd72ee5421849e48e2763
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-23 13:59:25 +0400

    Add App-with-layout.vue

---
+++ src/App-with-layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <LayoutContainer>
+    <main class="app">
+      <h1>Random Quote Generator</h1>
+      <QuoteView />
+    </main>
+  </LayoutContainer>
+</template>
+
+<script setup>
+import LayoutContainer from './components/LayoutContainer.vue';
+import QuoteView from './components/QuoteView.vue';
+</script>
+
+<style>.app{text-align:center;}</style>
